16ac4d6c6085bd6d1df8c2b259837d792731d0b7
MAC <my@MACdeAir.lan>
2023-04-29 14:23:38 +0800

    add .env.local and chat component

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
+import CustomHeader from "@/components/CustomHeader"
 function Chat() {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "chenyi",
+        "1234",
+    )
+    console.log("project id: ", import.meta.env)
     return (
-        <div>
-            Chat
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={(chat) => <CustomHeader chat={chat}></CustomHeader>}
+            />
         </div>
     )
 }
