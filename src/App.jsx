bae36ffde71d30563cda5978e6f8d2e678e93c5e
MAC <my@MACdeMacBook-Air.local>
2023-04-29 12:55:13 +0800

    add chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Chat from "./components/chat"
 function App() {
   return (
     <div className="app">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Chat />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
