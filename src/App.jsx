545715321c55ff72543857dc236e5023b02d4bbc
MAC <my@MACdeMacBook-Air.local>
2023-04-29 13:12:15 +0800

    add path config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Chat from "./components/chat"
+import Chat from "@/components/chat"
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Chat />}></Route>
+          <Route path="/chat" element={<Chat />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
