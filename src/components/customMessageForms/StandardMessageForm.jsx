e9e598836e275204d2a82806cf6f867509ad9821
MAC <my@MACdeMacBook-Air.local>
2023-04-30 09:17:59 +0800

    add standard message form preview

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,9 +1,27 @@
+import { XMarkIcon } from '@heroicons/react/24/solid';
 import React from 'react'
+import { useState } from 'react'
 
 function StandardMessageForm() {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [preview, setPreview] = useState("")
     return (
-        <div>
-            StandardMessageForm
+        <div className='message-form-container'>
+            {preview && (
+                <div className="mesage-form-preview">
+                    <img alt="message-form-preview"
+                        className='message-form-preview-image' src={preview}
+                        onLoad={() => URL.revokeObjectURL(preview)} />
+                </div>
+            )}
+            <XMarkIcon
+                className='message-form-icon-x'
+                onClick={() => {
+                    setPreview("")
+                    setAttachment("")
+                }}
+            />
         </div>
     )
 }
