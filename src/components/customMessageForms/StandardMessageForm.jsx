401237e26f02b7d5bbb4f19a95aabc179a81b80a
MAC <my@MACdeMacBook-Air.local>
2023-04-30 10:56:31 +0800

    send message

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,11 +1,31 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 import { useState } from 'react'
+import Dropzone from 'react-dropzone';
 
-function StandardMessageForm() {
+function StandardMessageForm({ props, activeChat }) {
+    console.log("props: ", props);
+    console.log("active chat: ", activeChat)
     const [message, setMessage] = useState("");
     const [attachment, setAttachment] = useState("");
     const [preview, setPreview] = useState("")
+    const handleChange = (e) => setMessage(e.target.value)
+    const handleSubmit = async () => {
+        console.log("handleSubmit props: ", props)
+        const date = new Date().toISOString().replace("T", ' ').replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`)
+        const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+        const form = {
+            attachments: at,
+            created: date,
+            sender_username: props.username,
+            text: message,
+            activeChatId: activeChat.id,
+        }
+
+        props.onSubmit(form);
+        setMessage("")
+        setAttachment("")
+    }
     return (
         <div className='message-form-container'>
             {preview && (
@@ -13,15 +33,52 @@ function StandardMessageForm() {
                     <img alt="message-form-preview"
                         className='message-form-preview-image' src={preview}
                         onLoad={() => URL.revokeObjectURL(preview)} />
+
+                    <XMarkIcon
+                        className='message-form-icon-x'
+                        onClick={() => {
+                            setPreview("")
+                            setAttachment("")
+                        }}
+                    />
                 </div>
             )}
-            <XMarkIcon
-                className='message-form-icon-x'
-                onClick={() => {
-                    setPreview("")
-                    setAttachment("")
-                }}
-            />
+            <div className='message-form'>
+                <div className='message-form-input-container'>
+                    <input className='message-form-input'
+                        type="text"
+                        value={message}
+                        onChange={handleChange}
+                        placeholder="Send a Message..."
+                    ></input>
+                </div>
+                <div className='message-form-icons'>
+                    <Dropzone
+                        acceptedFiles=".jpg,.jpeg,.png"
+                        multiple={false}
+                        onDrop={(acceptedFiles) => {
+                            setAttachment(acceptedFiles[0])
+                            setPreview(URL.createObjectURL(acceptedFiles[0]))
+                        }}>
+                        {({ getRootProps, getInputProps, open }) => (
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()}></input>
+                                <PaperClipIcon className='message-form-icon-clip'
+                                    onClick={open}
+                                >
+
+                                </PaperClipIcon>
+                            </div>
+                        )}
+                    </Dropzone>
+                    <hr className='vertical-line'></hr>
+                    <PaperAirplaneIcon className='message-form-icon-airplane'
+                        onClick={() => {
+                            setPreview("");
+                            handleSubmit()
+                        }}></PaperAirplaneIcon>
+                </div>
+            </div>
         </div>
     )
 }
