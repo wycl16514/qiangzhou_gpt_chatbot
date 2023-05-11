d941588fd7965612633465a72b89e6560ca151a1
MAC <my@MACdeAir.lan>
2023-05-11 18:46:12 +0800

    set radio to unchecked

--- src/components/exam/exam.jsx
+++ src/components/exam/exam.jsx
@@ -64,15 +64,16 @@ function Exam() {
 
                 {answers.map((item, index) => {
                     return (
-                        <div class="flex items-center mb-4">
-                            <input id="default-radio-1" type="radio" value="" name="default-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100
+                        <div className="flex items-center mb-4" key={index}>
+                            <input id="default-radio-1" type="radio" name="default-radio"
+                                className="w-4 h-4 text-blue-600 bg-gray-100
                               border-gray-300 focus:ring-blue-500 
                              dark:focus:ring-blue-600 
                              dark:ring-offset-gray-800 focus:ring-2
-                              dark:bg-gray-700 dark:border-gray-600" />
+                              dark:bg-gray-700 dark:border-gray-600"
+                            />
                             <label for="default-radio-1"
-                                class="ml-2 text-lg font-medium ">
+                                className="ml-2 text-lg font-medium ">
                                 {item}
                             </label>
                         </div>
